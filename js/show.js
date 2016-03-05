dbb5d3781e3fa257ff6ef22c16351d6b72a03632
xiaojiehao123 <327317942@qq.com>
2016-03-05 14:17:01 +0800

    change baiduyixia

--- js/show.js
+++ js/show.js
@@ -875,5 +875,11 @@ window.onload=function ()
 			}	
 		})
 	})();
+	
+	var oTxj_baidu=document.getElementById('txj_baidu');
+	var oTxj_bai_box=document.getElementById('txj_bai_box');
+	oTxj_baidu.onclick=function(){
+		oTxj_bai_box.style.display='block';
+	};
 
 }
